c4a1a2ce8109ea3f0643b0c83c4439d0dfaa7d3b
jakub-swiniarski <jakub.swiniarski2007@gmail.com>
2023-08-26 15:54:44 +0200

    updated player sprite

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,8 +4,8 @@
 Player::Player()
 {
     Player::setPosition(0, 0);
-    img.loadFromFile("assets/sprites/orzel.png");
-    Player::setScale(6.f, 6.f);
+    img.loadFromFile("assets/sprites/zywiec.png");
+    Player::setScale(4.f,4.f);
     Player::setTexture(img);
 }
 

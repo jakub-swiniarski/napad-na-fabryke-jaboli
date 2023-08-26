aec038a2359189105f760d6361ebb6f30964ea3d
Boper <85123713+Boper0@users.noreply.github.com>
2023-08-26 21:32:32 +0200

    ez

--- src/Player.cpp
+++ src/Player.cpp
@@ -5,15 +5,15 @@ Player::Player()
 {
     Player::setPosition(0, 0);
 
-    //standing
+    // standing
     imgU.loadFromFile("assets/sprites/zywiec/idle/zywiecU.png");
     imgD.loadFromFile("assets/sprites/zywiec/idle/zywiecD.png");
     imgR.loadFromFile("assets/sprites/zywiec/idle/zywiecR.png");
     imgL.loadFromFile("assets/sprites/zywiec/idle/zywiecL.png");
 
-    //walking
+    // walking
 
-    Player::setScale(4.f, 4.f);
+    Player::setScale(3.f, 3.f);
     Player::setTexture(imgD);
     speed = 255;
 }
@@ -27,7 +27,8 @@ void Player::update(float dt)
         Player::move(0.f, -speed * dt);
         Player::setTexture(imgU);
 
-        if(clock.getElapsedTime().asSeconds()>=0.3){
+        if (clock.getElapsedTime().asSeconds() >= 0.3)
+        {
             clock.restart();
         }
     }
@@ -36,26 +37,29 @@ void Player::update(float dt)
         Player::move(0.f, speed * dt);
         Player::setTexture(imgD);
 
-        if(clock.getElapsedTime().asSeconds()>=0.3){
+        if (clock.getElapsedTime().asSeconds() >= 0.3)
+        {
             clock.restart();
-        }         
+        }
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
     {
         Player::move(speed * dt, 0.f);
         Player::setTexture(imgR);
 
-        if(clock.getElapsedTime().asSeconds()>=0.3){
+        if (clock.getElapsedTime().asSeconds() >= 0.3)
+        {
             clock.restart();
-        } 
+        }
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
     {
         Player::move(-speed * dt, 0.f);
         Player::setTexture(imgL);
 
-        if(clock.getElapsedTime().asSeconds()>=0.3){
+        if (clock.getElapsedTime().asSeconds() >= 0.3)
+        {
             clock.restart();
-        } 
+        }
     }
 }

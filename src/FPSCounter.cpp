568f0a4398a6011b37b9e0a361b5884e498ced3c
Boper <85123713+Boper0@users.noreply.github.com>
2023-08-26 14:42:30 +0200

    Merge conflict

--- src/FPSCounter.cpp
+++ src/FPSCounter.cpp
@@ -1,12 +1,13 @@
 #include "../headers/FPSCounter.hpp"
 #include <SFML/Graphics/Color.hpp>
 
-FPSCounter::FPSCounter(){
-    fps=0;
-    visible=0;
+FPSCounter::FPSCounter()
+{
+    fps = 0;
+    visible = 0;
 
     FPSCounter::setString("0");
-    FPSCounter::setCharacterSize(36);
+    FPSCounter::setCharacterSize(24);
     FPSCounter::setFillColor(sf::Color::White);
-    FPSCounter::setPosition(5,-5);
+    FPSCounter::setPosition(5, -5);
 }

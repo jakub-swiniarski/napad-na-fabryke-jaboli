344820f3541cfe1041d94216c61bc6693e5b5363
jakub-swiniarski <jakub.swiniarski2007@gmail.com>
2023-09-08 21:54:19 +0200

    room vertical collisions for entities

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -12,12 +12,16 @@ void Entity::update(float dt){
         if(Entity::getPosition().x<=68){
             Entity::setPosition(68,Entity::getPosition().y);
         }
-         
         else if(Entity::getPosition().x+Entity::getTexture()->getSize().x*Entity::getScale().x>=1280-68){
             Entity::setPosition(1280-68-Entity::getTexture()->getSize().x*Entity::getScale().x,Entity::getPosition().y);
         }
 
         //vertical
-        //if(Entity::getPosition().y)
+        if(Entity::getPosition().y<=68){
+            Entity::setPosition(Entity::getPosition().x,68);
+        }
+        else if(Entity::getPosition().y+Entity::getTexture()->getSize().y*Entity::getScale().y>=720-68){
+            Entity::setPosition(Entity::getPosition().x,720-68-Entity::getTexture()->getSize().y*Entity::getScale().y);
+        }
     }
 }
